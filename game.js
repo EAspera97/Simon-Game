3d19c4e7df01d77f08caba5508907aa710a8c005
Enrique Aspera <enrique.alejandro.aspera@gmail.com>
2022-11-30 11:54:49 +0000

    Step 6 completed

--- game.js
+++ game.js
@@ -3,15 +3,14 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 $(".btn").on("click", function () {
-
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
 
 function nextSequence() {
-
     var randomNumber = Math.round(Math.random() * 3);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -22,7 +21,13 @@ function nextSequence() {
 }
 
 function playSound(name) {
-
     var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100)
 }
